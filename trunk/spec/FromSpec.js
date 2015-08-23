be4be4de04f50fa16a755453fcdf88299bce28e8
avejidah <avejidah@7e54a7ad-dcae-48d8-97b2-ac449ff2a034>
2015-08-23 03:56:11 +0000

    DataContext started.  From started.

---
+++ trunk/spec/FromSpec.js
@@ -0,0 +1,48 @@
+describe('From (SELECT query) test suite', function()
+{
+  'use strict';
+
+  var From     = require(__dirname + '/../query/From');
+  var Database = require(__dirname + '/../Database');
+  var db       = new Database(require(__dirname + '/resource/testDB.json'));
+
+  // Checks the constructor.
+  it('checks the constructor.', function()
+  {
+    expect(function()
+    {
+      new From(db, 'users');
+    }).not.toThrow();
+
+    expect(function()
+    {
+      new From(db, 'INVALID_NAME');
+    }).toThrowError('Table INVALID_NAME does not exist in database testDB.');
+  });
+
+  // Checks that a basic select without columns specified is correct.
+  it('checks that a basic select without columns specified is correct.', function()
+  {
+    var query = new From(db, 'users');
+
+    expect(query.getSQL()).toBe
+    (
+      'SELECT  `users`.`userID` AS `users.userID`, `users`.`firstName` AS `users.firstName`, `users`.`lastName` AS `users.lastName`\n' +
+      'FROM    `users` AS `users`'
+    );
+  });
+
+  // Checks that a basic select with columns specified is correct.
+  it('checks that a basic select with columns specified is correct.', function()
+  {
+    /*var query = new From(db, 'users')
+      .select(['users.firstName', 'users.lastName']);
+
+    // Note that the primary key is implicitly added.
+    expect(query.getSQL()).toBe
+    (
+      'SELECT  `users`.`firstName` AS `users.firstName`, `users`.`lastName` AS `users.lastName`, `users`.`userID` AS `users.userID`\n' +
+      'FROM    `users` AS `users`'
+    );*/
+  });
+});
