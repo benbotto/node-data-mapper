c9be9628873eab449003b22e249c055d91f3f81b
avejidah <avejidah@7e54a7ad-dcae-48d8-97b2-ac449ff2a034>
2015-11-28 17:27:09 +0000

    Base class for query executers.

---
+++ trunk/spec/QueryExecuterSpec.js
@@ -0,0 +1,49 @@
+describe('QueryExeucter test suite.', function()
+{
+  'use strict';
+
+  var QueryExecuter = require(__dirname + '/../query/QueryExecuter');
+  var qe;
+
+  beforeEach(function()
+  {
+    qe = new QueryExecuter();
+  });
+
+  // Checks that select is not implemented.
+  it('checks that select is not implemented.', function()
+  {
+    expect(function()
+    {
+      qe.select();
+    }).toThrowError('QueryExecuter::select not implemented.');
+  });
+
+  // Checks that update is not implemented.
+  it('checks that update is not implemented.', function()
+  {
+    expect(function()
+    {
+      qe.update();
+    }).toThrowError('QueryExecuter::update not implemented.');
+  });
+
+  // Checks that delete is not implemented.
+  it('checks that delete is not implemented.', function()
+  {
+    expect(function()
+    {
+      qe.delete();
+    }).toThrowError('QueryExecuter::delete not implemented.');
+  });
+
+  // Checks that insert is not implemented.
+  it('checks that insert is not implemented.', function()
+  {
+    expect(function()
+    {
+      qe.insert();
+    }).toThrowError('QueryExecuter::insert not implemented.');
+  });
+});
+
