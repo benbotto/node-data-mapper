74bf5689d3e833a8b4a0ca07f4f3a79bec0f578f
benbotto <benbotto@users.noreply.github.com>
2015-12-19 11:13:11 -0800

    Added an index for publishing.

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * This module just exports all the other modules.
+ */
+module.exports =
+{
+  Database:           require('./database/Database'),
+  Table:              require('./database/Table'),
+  Column:             require('./database/Column'),
+  DataContext:        require('./datacontext/DataContext'),
+  MySQLDataContext:   require('./datacontext/MySQLDataContext'),
+  Escaper:            require('./query/Escaper'),
+  MySQLEscaper:       require('./query/MySQLEscaper'),
+  ConditionParser:    require('./query/ConditionParser'),
+  ConditionLexer:     require('./query/ConditionLexer'),
+  ConditionCompiler:  require('./query/ConditionCompiler'),
+  From:               require('./query/From'),
+  QueryExecuter:      require('./query/QueryExecuter'),
+  MySQLQueryExecuter: require('./query/MySQLQueryExecuter'),
+  Schema:             require('./datamapper/Schema'),
+  DataMapper:         require('./datamapper/DataMapper')
+};
+
