ebc0156aad3e899ce070b53d27f29d0b880b7a33
benbotto <benbotto@users.noreply.github.com>
2015-12-19 17:52:51 -0800

    CHORE: comment removal.

--- example/retrieve/selectColumns.js
+++ example/retrieve/selectColumns.js
@@ -7,12 +7,9 @@ var query = bikeShopDC
   .from('bike_shops')
   .select('bikeShops.bikeShopID', 'bikeShops.name');
 
-// This is the query that will be executed.
 console.log('Query:');
 console.log(query.toString(), '\n');
 
-// Executing a query returns a promise, as defined by the deferred API.
-// https://www.npmjs.com/package/deferred
 query.execute().then(function(result)
 {
   console.log('Result:');
@@ -23,6 +20,5 @@ query.execute().then(function(result)
   console.log(err);
 }).finally(function()
 {
-  // Close the connection.
   bikeShopDC.getQueryExecuter().getConnectionPool().end();
 });
