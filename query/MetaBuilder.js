204d3537ea7db8df4bdaf45a06cc14eb2be6d82f
benbotto <benbotto@users.noreply.github.com>
2016-02-23 09:02:59 -0800

    MetaBuilder no longer handles recursing.

--- query/MetaBuilder.js
+++ query/MetaBuilder.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var traverse = require('./modelTraverse');
-
 /**
  * This class is used for building meta data (columns, values, and models) for
  * queries that persist (UPDATE and INSERT).
@@ -10,13 +8,6 @@ function MetaBuilder()
 {
 }
 
-MetaBuilder.RECURSE_TYPE =
-{
-  NONE:          'none',
-  DEPTH_FIRST:   'depth-first',
-  BREADTH_FIRST: 'breadth-first'
-};
-
 /**
  * Build an array of model, and key-value pairs.  This meta is used when
  * creating an INSERT or UPDATE query.  This method does not recurse into
@@ -24,91 +15,41 @@ MetaBuilder.RECURSE_TYPE =
  * @param database A Database instance describing the database.
  * @param tableAlias The alias of a database table.  If this does not existing
  *        in the database instance then the model is ignored.
- * @param model A model object or array to persist.  Each key should correspond 
- *        to a column alias.
+ * @param model A model object to persist.  Each key should correspond to a
+ *        column alias.
  */
-MetaBuilder.prototype._flattenModel = function(database, tableAlias, model)
+MetaBuilder.prototype.buildMeta = function(database, tableAlias, model)
 {
-  var table, columns, val, fields;
-  var meta = [];
+  var table, columns, val;
+  var fields = [];
+  var meta   = {};
 
   // If the tableAlias does not exist in the database it is ignored.
   if (!database.isTableAlias(tableAlias))
-    return meta;
-
-  if (!(model instanceof Array))
-    model = [model];
+    return null;
 
   table   = database.getTableByAlias(tableAlias);
   columns = table.getColumns();
+  fields  = [];
 
-  for (var i = 0; i < model.length; ++i)
+  for (var i = 0; i < columns.length; ++i)
   {
-    fields = [];
-
-    for (var j = 0; j < columns.length; ++j)
+    // undefined values are skipped.
+    if ((val = model[columns[i].getAlias()]) !== undefined)
     {
-      // undefined values are skipped.
-      if ((val = model[i][columns[j].getAlias()]) !== undefined)
-      {
-        // If there is a converter associated with this column, use it.
-        if (val !== null && columns[j].getConverter().onSave)
-          val = columns[j].getConverter().onSave(val);
-        fields.push({columnName: columns[j].getName(), value: val});
-      }
+      // If there is a converter associated with this column, use it.
+      if (val !== null && columns[i].getConverter().onSave)
+        val = columns[i].getConverter().onSave(val);
+      fields.push({columnName: columns[i].getName(), value: val});
     }
-
-    meta.push
-    ({
-      model:     model[i],
-      tableName: table.getName(),
-      fields:    fields
-    });
   }
 
-  return meta;
-};
-
-/**
- * Build an array of model, and key-value pairs.  This meta is used when
- * creating an INSERT or UPDATE query.
- * @param database A Database instance describing the database.
- * @param model A model object to persist.  Each key in the object should be a
- *        table alias.  The value associated with the key should be an object
- *        (or an array of objects) wherein each key corresponds to a column
- *        alias.
- * @param recurseType One of 'none', 'depth-first', or 'breadth-first'.  How
- *        to traverse into sub models.
- */
-MetaBuilder.prototype.buildMeta = function(database, model, recurseType)
-{
-  var meta = [];
-
-  if (recurseType === MetaBuilder.RECURSE_TYPE.NONE || recurseType === undefined)
+  meta =
   {
-    // No recursion, just build the meta for the top-level model(s).
-    for (var tblAlias in model)
-      meta = meta.concat(this._flattenModel(database, tblAlias, model[tblAlias]));
-  }
-  else if (recurseType === MetaBuilder.RECURSE_TYPE.DEPTH_FIRST)
-  {
-    // Traverse the model using a depth-first traversal.  For each model
-    // call _flattenModel.
-    traverse.depthFirst(model, function(tblAlias, model)
-    {
-      meta = meta.concat(this._flattenModel(database, tblAlias, model));
-    }.bind(this));
-  }
-  else if (recurseType === MetaBuilder.RECURSE_TYPE.BREADTH_FIRST)
-  {
-    // Traverse the model using a depth-first traversal.
-    traverse.breadthFirst(model, function(tblAlias, model)
-    {
-      meta = meta.concat(this._flattenModel(database, tblAlias, model));
-    }.bind(this));
-  }
-  else
-    throw new Error('Invalid recurseType.');
+    model:     model,
+    tableName: table.getName(),
+    fields:    fields
+  };
 
   return meta;
 };
--- query/MetaBuilderSpec.js
+++ query/MetaBuilderSpec.js
@@ -12,305 +12,58 @@ describe('MetaBuilder test suite.', function()
     // Checks a basic model.
     it('checks a basic model.', function()
     {
-      var meta = metaBuilder.buildMeta(db,
-      {
-        users: {first: 'Sandy', last: 'Perkins'}
-      });
+      var meta = metaBuilder.buildMeta(db, 'users', {first: 'Sandy', last: 'Perkins'});
 
       expect(meta).toEqual
-      ([
-        {
-          model: {first: 'Sandy', last: 'Perkins'},
-          tableName: 'users',
-          fields:
-          [
-            {columnName: 'firstName', value: 'Sandy'},
-            {columnName: 'lastName',  value: 'Perkins'}
-          ]
-        }
-      ]);
+      ({
+        model: {first: 'Sandy', last: 'Perkins'},
+        tableName: 'users',
+        fields:
+        [
+          {columnName: 'firstName', value: 'Sandy'},
+          {columnName: 'lastName',  value: 'Perkins'}
+        ]
+      });
     });
 
     // Checks a basic model with a null value.
     it('checks a basic model with a null value.', function()
     {
-      var meta = metaBuilder.buildMeta(db,
-      {
-        users: {first: 'Sandy', last: null}
-      });
+      var meta = metaBuilder.buildMeta(db, 'users', {first: 'Sandy', last: null});
 
       expect(meta).toEqual
-      ([
-        {
-          model: {first: 'Sandy', last: null},
-          tableName: 'users',
-          fields:
-          [
-            {columnName: 'firstName', value: 'Sandy'},
-            {columnName: 'lastName',  value: null}
-          ]
-        }
-      ]);
+      ({
+        model: {first: 'Sandy', last: null},
+        tableName: 'users',
+        fields:
+        [
+          {columnName: 'firstName', value: 'Sandy'},
+          {columnName: 'lastName',  value: null}
+        ]
+      });
     });
 
     // Checks a model with a non-table-alias property.
     it('checks a model with a non-table-alias property.', function()
     {
-      var meta = metaBuilder.buildMeta(db,
-      {
-        foo: {bar: 'baz'}
-      });
-
-      expect(meta).toEqual([]);
+      var meta = metaBuilder.buildMeta(db, 'foo', {bar: 'baz'});
+      expect(meta).toBeNull();
     });
 
-    // Checks an array model with one element.
-    it('checks an array model with one element.', function()
+    // Checks a model with a non-column-alias property.
+    it('checks a model with a non-column-alias property.', function()
     {
-      var meta = metaBuilder.buildMeta(db,
-      {
-        users: [{first: 'Sandy', last: 'Perkins'}]
-      });
+      var meta = metaBuilder.buildMeta(db, 'users', {first: 'Sandy', soup: 'Onion'});
 
       expect(meta).toEqual
-      ([
-        {
-          model: {first: 'Sandy', last: 'Perkins'},
-          tableName: 'users',
-          fields:
-          [
-            {columnName: 'firstName', value: 'Sandy'},
-            {columnName: 'lastName',  value: 'Perkins'}
-          ]
-        }
-      ]);
-    });
-
-    // Checks an array model with multiple elements.
-    it('checks an array model with multiple elements.', function()
-    {
-      var meta = metaBuilder.buildMeta(db,
-      {
-        users:
+      ({
+        model: {first: 'Sandy', soup: 'Onion'},
+        tableName: 'users',
+        fields:
         [
-          {first: 'Sandy', last: 'Perkins'},
-          {first: 'Jose',  last: 'Quervo'},
-          {first: 'Rose',  last: 'Marker'}
+          {columnName: 'firstName', value: 'Sandy'}
         ]
       });
-
-      expect(meta).toEqual
-      ([
-        {
-          model: {first: 'Sandy', last: 'Perkins'},
-          tableName: 'users',
-          fields:
-          [
-            {columnName: 'firstName', value: 'Sandy'},
-            {columnName: 'lastName', value: 'Perkins'}
-          ]
-        },
-        {
-          model: {first: 'Jose', last: 'Quervo'},
-          tableName: 'users',
-          fields:
-          [
-            {columnName: 'firstName', value: 'Jose'},
-            {columnName: 'lastName', value: 'Quervo'}
-          ]
-        },
-        {
-          model: {first: 'Rose', last: 'Marker'},
-          tableName: 'users',
-          fields:
-          [
-            {columnName: 'firstName', value: 'Rose'},
-            {columnName: 'lastName', value: 'Marker'}
-          ]
-        }
-      ]);
-    });
-
-    // Checks a model with two table aliases.
-    it('checks a model with two table aliases.', function()
-    {
-      var meta = metaBuilder.buildMeta(db,
-      {
-        users: {first: 'Sandy', last: 'Perkins'},
-        phoneNumbers: {userID: 12, phoneNumber: '999-888-7777', type: 'home'}
-      });
-
-      expect(meta).toEqual
-      ([
-        {
-          model: {first: 'Sandy', last: 'Perkins'},
-          tableName: 'users',
-          fields:
-          [
-            {columnName: 'firstName', value: 'Sandy'},
-            {columnName: 'lastName', value: 'Perkins'}
-          ]
-        },
-        {
-          model: {userID: 12, phoneNumber: '999-888-7777', type: 'home'},
-          tableName: 'phone_numbers',
-          fields:
-          [
-            {columnName: 'userID', value: 12},
-            {columnName: 'phoneNumber', value: '999-888-7777'},
-            {columnName: 'type', value: 'home'}]
-        }
-      ]);
-    });
-
-    // Checks a model with a nested model.
-    it('checks a model with a nested model.', function()
-    {
-      var meta = metaBuilder.buildMeta(db,
-      {
-        users:
-        {
-          first: 'Sandy',
-          last: 'Perkins',
-          phoneNumbers:
-          [
-            {userID: 12, phoneNumber: '999-888-7777', type: 'home'}
-          ]
-        }
-      });
-
-      expect(meta).toEqual
-      ([
-        {
-          model:
-          {
-            first: 'Sandy',
-            last: 'Perkins',
-            phoneNumbers:
-            [
-              {userID: 12, phoneNumber: '999-888-7777', type: 'home'}
-            ]
-          },
-          tableName: 'users',
-          fields:
-          [
-            {columnName: 'firstName', value: 'Sandy'},
-            {columnName: 'lastName', value: 'Perkins'}
-          ]
-        }
-      ]);
-    });
-
-    // Checks that an invalid recurseType throws.
-    it('checks that an invalid recurseType throws.', function()
-    {
-      expect(function()
-      {
-        metaBuilder.buildMeta(db, {}, 'invalid');
-      }).toThrowError('Invalid recurseType.');
-    });
-  });
-
-  // Note: The recursive traversal is mainly tested in modelTraverseSpec.js.
-  describe('MetaBuilder depth-first recurse mode.', function()
-  {
-    // Checks a model with a nested model.
-    it('checks a model with a nested model.', function()
-    {
-      var meta = metaBuilder.buildMeta(db,
-      {
-        users:
-        {
-          first: 'Sandy',
-          last: 'Perkins',
-          phoneNumbers:
-          [
-            {userID: 12, phoneNumber: '999-888-7777', type: 'home'}
-          ]
-        }
-      }, 'depth-first');
-
-      expect(meta).toEqual
-      ([
-        {
-          model: {userID: 12, phoneNumber: '999-888-7777', type: 'home'},
-          tableName: 'phone_numbers',
-          fields:
-          [
-            {columnName: 'userID', value: 12},
-            {columnName: 'phoneNumber', value: '999-888-7777'},
-            {columnName: 'type', value: 'home'}
-          ]
-        },
-        {
-          model:
-          {
-            first: 'Sandy',
-            last: 'Perkins',
-            phoneNumbers:
-            [
-              {userID: 12, phoneNumber: '999-888-7777', type: 'home'}
-            ]
-          },
-          tableName: 'users',
-          fields:
-          [
-            {columnName: 'firstName', value: 'Sandy'},
-            {columnName: 'lastName', value: 'Perkins'}
-          ]
-        }
-      ]);
-    });
-  });
-
-  describe('MetaBuilder breadth-first recurse mode.', function()
-  {
-    // Checks a model with a nested model.
-    it('checks a model with a nested model.', function()
-    {
-      var meta = metaBuilder.buildMeta(db,
-      {
-        users:
-        {
-          first: 'Sandy',
-          last: 'Perkins',
-          phoneNumbers:
-          [
-            {userID: 12, phoneNumber: '999-888-7777', type: 'home'}
-          ]
-        }
-      }, 'breadth-first');
-
-      expect(meta).toEqual
-      ([
-        {
-          model:
-          {
-            first: 'Sandy',
-            last: 'Perkins',
-            phoneNumbers:
-            [
-              {userID: 12, phoneNumber: '999-888-7777', type: 'home'}
-            ]
-          },
-          tableName: 'users',
-          fields:
-          [
-            {columnName: 'firstName', value: 'Sandy'},
-            {columnName: 'lastName', value: 'Perkins'}
-          ]
-        },
-        {
-          model: {userID: 12, phoneNumber: '999-888-7777', type: 'home'},
-          tableName: 'phone_numbers',
-          fields:
-          [
-            {columnName: 'userID', value: 12},
-            {columnName: 'phoneNumber', value: '999-888-7777'},
-            {columnName: 'type', value: 'home'}
-          ]
-        },
-      ]);
     });
   });
 });
