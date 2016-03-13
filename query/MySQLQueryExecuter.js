2c60b193fa2bbdec3cc8436a9a41630ff54fc1b8
benbotto <benbotto@users.noreply.github.com>
2016-03-13 15:46:22 -0700

    delete method implemented.

--- query/MySQLQueryExecuter.js
+++ query/MySQLQueryExecuter.js
@@ -24,8 +24,8 @@ MySQLQueryExecuter.prototype.getConnectionPool = function()
  * @param query The SQL to execute.  The query is expected to be escaped.
  * @param callback The callback function that is called when the query is executed.
  *        function callback(err, results)
- *        The results are an array, one entry per row, of objects
- *        that are pairs of column->value.
+ *        The results are an array, one entry per row, of objects that are
+ *        pairs of column->value.
  */
 MySQLQueryExecuter.prototype.select = function(query, callback)
 {
@@ -37,9 +37,21 @@ MySQLQueryExecuter.prototype.select = function(query, callback)
  * @param query The SQL to execute.  The query is expected to be escaped.
  * @param callback The callback function that is called when the query is executed.
  *        function callback(err, result)
- *        If available, the result object should have an insertId property.
+ *        If available, the result object shall have an insertId property.
  */
 MySQLQueryExecuter.prototype.insert = function(query, callback)
+{
+  this._pool.query(query, callback);
+};
+
+/**
+ * Execute a delete query.
+ * @param query The SQL to execute.  The query is expected to be escaped.
+ * @param callback The callback function that is called when the query is executed.
+ *        function callback(err, result)
+ *        result shall have an affectedRows property.
+ */
+MySQLQueryExecuter.prototype.delete = function(query, callback)
 {
   this._pool.query(query, callback);
 };
--- query/MySQLQueryExecuterSpec.js
+++ query/MySQLQueryExecuterSpec.js
@@ -32,5 +32,15 @@ describe('MySQLQueryExecuter test suite.', function()
 
     expect(con.query.calls.argsFor(0)).toEqual([query, callback]);
   });
+
+  // Checks the delete method.
+  it('checks the delete method.', function()
+  {
+    var callback = null;
+    var query = 'DELETE FROM users WHERE userID = 1';
+    qe.insert(query, callback);
+
+    expect(con.query.calls.argsFor(0)).toEqual([query, callback]);
+  });
 });
 
