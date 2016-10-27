ed54f456cb34978fcefb0c74abb088f8d5582c40
benbotto <benbotto@users.noreply.github.com>
2016-10-26 20:10:41 -0700

    Column changed to preserve the passed-in shema's properties.  Removed accessors.

--- database/ColumnSpec.js
+++ database/ColumnSpec.js
@@ -1,121 +1,33 @@
-describe('Column test suite', function()
-{
+describe('Column()', function() {
   'use strict';
 
-  var Column = require('./Column');
+  const Column = require('./Column');
 
-  // Checks the constructor.
-  it('checks the constructor.', function()
-  {
-    var col = new Column({name: 'TestCol'});
-    expect(col.getName()).toBe('TestCol');
-    expect(col.getAlias()).toBe('TestCol');
-    expect(col.isPrimary()).toBe(false);
-    expect(col.getConverter()).toEqual({});
+  /**
+   * Constructor.
+   */
+  describe('.constructor()', function() {
+    it('checks the constructor.', function() {
+      const col = new Column({name: 'TestCol'});
+      expect(col.name).toBe('TestCol');
+      expect(col.mapTo).toBe('TestCol');
+      expect(col.isPrimary).toBe(false);
+      expect(col.converter).toEqual({});
 
-    var col2 = new Column({name: 'TestCol2', alias: 'test', isPrimary: false});
-    expect(col2.getName()).toBe('TestCol2');
-    expect(col2.getAlias()).toBe('test');
-    expect(col2.isPrimary()).toBe(false);
-    expect(col2.getConverter()).toEqual({});
+      const col2 = new Column({name: 'TestCol2', mapTo: 'test', isPrimary: false});
+      expect(col2.name).toBe('TestCol2');
+      expect(col2.mapTo).toBe('test');
+      expect(col2.isPrimary).toBe(false);
+      expect(col2.converter).toEqual({});
 
-    var col3 = new Column({name: 'TestCol3', isPrimary: true});
-    expect(col3.isPrimary()).toBe(true);
+      const col3 = new Column({name: 'TestCol3', isPrimary: true});
+      expect(col3.isPrimary).toBe(true);
 
-    var converter = {};
-    var col4 = new Column({name: 'TestCol4', converter: converter});
+      const converter = {};
+      const col4 = new Column({name: 'TestCol4', converter: converter});
 
-    expect(col4.getConverter()).toBe(converter);
-  });
-
-  // Checks that the data type can be retrieved.
-  it('checks that the data type can be retrieved.', function()
-  {
-    var col = new Column({name: 'personName'});
-
-    expect(col.getDataType()).toBeNull();
-
-    col = new Column({name: 'personName', dataType: 'varchar'});
-    expect(col.getDataType()).toBe('varchar');
-  });
-
-  // Checks that the data type can be retrieved.
-  it('checks that the data type can be retrieved.', function()
-  {
-    var col = new Column({name: 'personName'});
-
-    expect(col.getMaxLength()).toBeNull();
-
-    col = new Column({name: 'personName', maxLength: 100});
-    expect(col.getMaxLength()).toBe(100);
-  });
-
-  // Checks the default value.
-  it('checks the default value.', function()
-  {
-    var col = new Column({name: 'personName'});
-
-    expect(col.getDefaultValue()).toBeNull();
-
-    col = new Column({name: 'personName', defaultValue: 'asdf'});
-    expect(col.getDefaultValue()).toBe('asdf');
-  });
-
-  // Checks the isNullable method.
-  it('checks the isNullable method.', function()
-  {
-    var col = new Column({name: 'personName'});
-
-    expect(col.isNullable()).toBe(true);
-
-    col = new Column({name: 'personName', isNullable: true});
-    expect(col.isNullable()).toBe(true);
-    col = new Column({name: 'personName', isNullable: false});
-    expect(col.isNullable()).toBe(false);
-  });
-
-  // Checks the toObject method.
-  it('checks the toObject method.', function()
-  {
-    var colDef = {name: 'TestCol'};
-    var col = new Column(colDef);
-    expect(col.toObject()).toEqual
-    ({
-      name: 'TestCol',
-      alias: 'TestCol',
-      isPrimary: false,
-      converter: {},
-      isNullable: true,
-      dataType: null,
-      maxLength: null,
-      defaultValue: null
+      expect(col4.converter).toBe(converter);
     });
-
-    colDef = 
-    {
-      name:         'createdOn',
-      alias:        'created',
-      dataType:     'timestamp',
-      maxLength:    100,
-      defaultValue: 'CURRENT_TIMESTAMP',
-      converter:
-      {
-        onSave:     {},
-        onRetrieve: {}
-      },
-      isPrimary:    true,
-      isNullable:   false
-    };
-    col = new Column(colDef);
-    expect(col.toObject()).toEqual(colDef);
-  });
-
-  // Checks the clone method.
-  it('checks the clone method.', function()
-  {
-    var col = new Column({name: 'userID', alias: 'ID', isPrimary: true, converter: {}});
-    var clone = col.clone();
-
-    expect(col.toObject()).toEqual(clone.toObject());
   });
 });
+
