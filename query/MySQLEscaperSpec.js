01ad19e6cdb07401eb8997f992246b622d06d759
benbotto <benbotto@users.noreply.github.com>
2016-11-01 15:10:07 -0700

    Spec names updadated to match project conventions.

--- query/MySQLEscaperSpec.js
+++ query/MySQLEscaperSpec.js
@@ -1,51 +1,53 @@
-xdescribe('MySQLEscaper test suite.', function()
-{
+describe('MySQLEscaper()', function() {
   'use strict';
 
-  var Escaper      = require('./Escaper');
-  var MySQLEscaper = require('./MySQLEscaper');
-  var escaper      = new MySQLEscaper();
+  const insulin      = require('insulin');
+  const Escaper      = insulin.get('ndm_Escaper');
+  const MySQLEscaper = insulin.get('ndm_MySQLEscaper');
+  const escaper      = new MySQLEscaper();
 
-  xdescribe('constructor test suite.', function()
-  {
-    // Makes sure that MySQLEscaper extends Escaper.
-    it('makes sure that MySQLEscaper extends Escaper.', function()
-    {
+  /**
+   * Ctor.
+   */
+  describe('.constructor().', function() {
+    it('extends Escaper.', function() {
       expect(escaper instanceof Escaper).toBe(true);
       expect(escaper instanceof MySQLEscaper).toBe(true);
     });
   });
 
-  xdescribe('property escape test suite.', function()
-  {
-    // Escapes a property.
-    it('escapes a property.', function()
-    {
+  /**
+   * Escape property.
+   */
+  describe('.escapeProperty()', function() {
+    it('escapes strings.', function() {
       expect(escaper.escapeProperty('name')).toBe('`name`');
     });
+
+    it('preserves dots.', function() {
+      expect(escaper.escapeProperty('my.name')).toBe('`my.name`');
+    });
+
+    // Note: see mysql.escapeId.
   });
 
-  xdescribe('literal escape test suite.', function()
-  {
-    // Escapes a string.
-    it('escapes strings.', function()
-    {
+  /**
+   * Escape literal.
+   */
+  describe('.escapeLiteral()', function() {
+    it('escapes strings.', function() {
       expect(escaper.escapeLiteral('')).toBe('\'\'');
       expect(escaper.escapeLiteral('Sue')).toBe('\'Sue\'');
       expect(escaper.escapeLiteral("Dali's Mom's Deli")).toBe("'Dali\\'s Mom\\'s Deli'");
     });
 
-    // Escapes numbers
-    it('escapes numbers.', function()
-    {
+    it('escapes numbers.', function() {
       expect(escaper.escapeLiteral(32)).toBe('32');
       expect(escaper.escapeLiteral(1.2)).toBe('1.2');
       expect(escaper.escapeLiteral(0)).toBe('0');
     });
 
-    // Escapes null and undefined.
-    it('escapes null and undefined.', function()
-    {
+    it('escapes null and undefined as NULL.', function() {
       expect(escaper.escapeLiteral(null)).toBe('NULL');
       expect(escaper.escapeLiteral(undefined)).toBe('NULL');
     });
@@ -53,11 +55,12 @@ xdescribe('MySQLEscaper test suite.', function()
     // Note: See node-mysql for escape details.
   });
 
-  xdescribe('fully-qualified column escape test suite.', function()
-  {
+  /**
+   * Escape FQC.
+   */
+  describe('.escapeFullqyQualifiedColumn()', function() {
     // Escapes a fully-qualified column.
-    it('escapes a fully-qualified column.', function()
-    {
+    it('escapes the table and the column independently.', function() {
       expect(escaper.escapeFullyQualifiedColumn('users.firstName')).toBe('`users`.`firstName`');
       expect(escaper.escapeFullyQualifiedColumn('users.first.Name')).toBe('`users`.`first.Name`');
       expect(escaper.escapeFullyQualifiedColumn('phone_numbers.phoneNumber')).toBe('`phone_numbers`.`phoneNumber`');
