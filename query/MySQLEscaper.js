8e6bbe390f36ff81a54f440bfb85b45c6274f16d
benbotto <benbotto@users.noreply.github.com>
2016-11-02 10:48:08 -0700

    Minor doc update (extends doc).

--- query/MySQLEscaper.js
+++ query/MySQLEscaper.js
@@ -4,7 +4,10 @@ require('insulin').factory('ndm_MySQLEscaper',
   ['mysql', 'ndm_Escaper'], ndm_MySQLEscaperProducer);
 
 function ndm_MySQLEscaperProducer(mysql, Escaper) {
-  /** Helper class for escaping parts of a query under MySQL. */
+  /**
+   * Helper class for escaping parts of a query under MySQL.
+   * @extends Escaper
+   */
   class MySQLEscaper extends Escaper {
     /**
      * Initialize the escaper.
