0798b36a7939edafbf209f3bb11fc19a671ed0cb
benbotto <benbotto@users.noreply.github.com>
2016-11-12 13:23:17 -0800

    Option for array destructuring.

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -10,7 +10,8 @@ module.exports = function(grunt, scripts) {
       undef:     true,
       unused:    true,
       node:      true,
-      esnext:    true
+      esnext:    true,
+      elision:   true
     },
 
     /* Get the lint out of all app files. */
