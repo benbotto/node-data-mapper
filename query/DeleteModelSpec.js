bf7e1dbe823a5357c576ca9e10208016d16998a0
Ben Botto <benjamin.botto@gmail.com>
2017-01-01 13:58:34 -0800

    Tests updated to use driver-specific implementations of createQuery().

--- query/DeleteModelSpec.js
+++ query/DeleteModelSpec.js
@@ -2,7 +2,6 @@ describe('DeleteModel()', function() {
   'use strict';
 
   const insulin      = require('insulin');
-  const DeleteModel  = insulin.get('ndm_DeleteModel');
   const MySQLEscaper = insulin.get('ndm_MySQLEscaper');
   const db           = insulin.get('ndm_testDB');
   const escaper      = new MySQLEscaper();
@@ -11,44 +10,17 @@ describe('DeleteModel()', function() {
   beforeEach(() => qryExec = jasmine.createSpyObj('qryExec', ['delete']));
 
   /**
-   * To string.
-   */
-  describe('.toString()', function() {
-    it('generates the correct sql for a single model.', function() {
-      const del = new DeleteModel(db, escaper, qryExec, {users: {ID: 1}});
-
-      expect(del.toString()).toBe(
-        'DELETE  `users`\n' +
-        'FROM    `users` AS `users`\n' +
-        'WHERE   (`users`.`userID` = :users_userID_0)'
-      );
-    });
-
-    it('generates the correct sql for multiple models.', function() {
-      const users = {users: [{ID: 1}, {ID: 3}]};
-      const del = new DeleteModel(db, escaper, qryExec, users);
-
-      expect(del.toString()).toBe(
-        'DELETE  `users`\n' +
-        'FROM    `users` AS `users`\n' +
-        'WHERE   (`users`.`userID` = :users_userID_0);\n\n' +
-
-        'DELETE  `users`\n' +
-        'FROM    `users` AS `users`\n' +
-        'WHERE   (`users`.`userID` = :users_userID_0)'
-      );
-    });
-  });
-
-  /**
    * Execute.
    */
   describe('.execute()', function() {
+    // MySQLDeleteModel is used because it has an impl of buildQuery.
+    const MySQLDeleteModel = insulin.get('ndm_MySQLDeleteModel');
+
     it('can delete a single model using QueryExecuter.delete().', function() {
       qryExec.delete.and.callFake((query, params, callback) =>
         callback(null, {affectedRows: 1}));
 
-      new DeleteModel(db, escaper, qryExec, {users: {ID: 14}})
+      new MySQLDeleteModel(db, escaper, qryExec, {users: {ID: 14}})
         .execute()
         .then(result => expect(result.affectedRows).toBe(1))
         .catch(() => expect(true).toBe(false))
@@ -62,7 +34,7 @@ describe('DeleteModel()', function() {
       qryExec.delete.and.callFake((query, params, callback) =>
         callback(null, {affectedRows: 1}));
 
-      new DeleteModel(db, escaper, qryExec, {users: [{ID: 14}, {ID: 33}]})
+      new MySQLDeleteModel(db, escaper, qryExec, {users: [{ID: 14}, {ID: 33}]})
         .execute()
         .then(result => expect(result.affectedRows).toBe(2))
         .catch(() => expect(true).toBe(false))
@@ -74,7 +46,7 @@ describe('DeleteModel()', function() {
     it('propagates query execution errors.', function() {
       qryExec.delete.and.callFake((query, params, callback) => callback('FAILURE'));
 
-      new DeleteModel(db, escaper, qryExec, {users: [{ID: 14}, {ID: 33}]})
+      new MySQLDeleteModel(db, escaper, qryExec, {users: [{ID: 14}, {ID: 33}]})
         .execute()
         .then(() => expect(true).toBe(false))
         .catch(err => expect(err).toBe('FAILURE'))
