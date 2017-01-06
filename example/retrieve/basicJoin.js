9a7b002029348b7a5e3058163648627d2573af7d
benbotto <benbotto@users.noreply.github.com>
2017-01-05 18:29:01 -0800

    Basic join example updated.

--- example/retrieve/basicJoin.js
+++ example/retrieve/basicJoin.js
@@ -1,28 +1,40 @@
 'use strict';
 
-var util       = require('util');
-var bikeShopDC = require('../bikeShopDataContext');
+const MySQLDriver = require('node-data-mapper-mysql').MySQLDriver;
+const driver      = new MySQLDriver(require('../bikeShopConOpts.json'));
 
-// Find all employees that have received bonuses.
-var query = bikeShopDC
-  .from('staff')
-  .innerJoin({table: 'bonuses', parent: 'staff', on: {$eq: {'staff.staffID':'bonuses.staffID'}}})
-  .select('staff.staffID', 'staff.firstName', 'staff.lastName', 'bonuses.bonusID', 'bonuses.amount');
+driver
+  .initialize()
+  .then(runQuery)
+  .then(printResult)
+  .catch(console.error)
+  .finally(() => driver.end());
 
-console.log('Query:');
-console.log(query.toString(), '\n');
+function runQuery(dataContext) {
+  // Find all employees that have received bonuses.
+  const query = dataContext
+    .from('staff s')
 
-query.execute()
-  .then(function(result)
-  {
-    console.log('Result:');
-    console.log(util.inspect(result, {depth: null}));
-  })
-  .catch(function(err)
-  {
-    console.log(err);
-  })
-  .finally(function()
-  {
-    bikeShopDC.getQueryExecuter().getConnectionPool().end();
-  });
+    // Join in bonuses as a child of staff.  Implicit join predicates are
+    // possible when there is a exactly one relationship between two tables.
+    // In this case bonuses has a staffID column that is a foreign key pointing
+    // at staff.staffID.
+    .innerJoin('s.bonuses b')
+
+    // Limit the selection to these columns.
+    .select('s.staffID', 's.firstName', 's.lastName', 'b.bonusID', 'b.amount');
+
+  console.log('Query:');
+  console.log(query.toString(), '\n');
+
+  return query
+    .execute();
+}
+
+function printResult(result) {
+  const util = require('util');
+
+  console.log('Result:');
+  console.log(util.inspect(result, {depth: null}));
+}
+
