45b5ae347bd83fa40d57a5cbe4ac96de5a8c1a6e
Ben Botto <benjamin.botto@gmail.com>
2017-01-14 18:11:41 -0800

    Advanced condition example.

--- example/retrieve/advancedWhere.js
+++ example/retrieve/advancedWhere.js
@@ -1,35 +1,71 @@
 'use strict';
 
-var bikeShopDC = require('../bikeShopDataContext');
+const MySQLDriver = require('node-data-mapper-mysql').MySQLDriver;
+const driver      = new MySQLDriver(require('../bikeShopConOpts.json'));
+const util        = require('util');
 
-// Find all employees that can rent cars.
-var query = bikeShopDC
-  .from('staff')
-  .where
-  ({
-    $or:
-    [
-      {$and: [{$eq: {'staff.sex':':male'}},   {$gte: {'staff.age':25}}]},
-      {$and: [{$eq: {'staff.sex':':female'}}, {$gte: {'staff.age':23}}]}
+driver
+  .initialize()
+  .then(runQuery)
+  .then(printResult)
+  .catch(console.error)
+  .finally(() => driver.end());
+
+/**
+ * Get a {@link From} instance which is a combination of staff, staff bonuses,
+ * and place of employment.
+ */
+function retrieveStaffQuery(dataContext) {
+  return dataContext
+    .from('staff')
+    .leftOuterJoin('staff.bonuses')
+    .innerJoin('staff.bike_shops');
+}
+
+function runQuery(dataContext) {
+  // Condition object to find all staff that are either:
+  // 1) male and over the age of 25, or
+  // 2) female and over the age of 23.
+  const cond = {
+    $or: [
+      {
+        // Male and over the age of 25.
+        $and: [
+          {$eq: {'staff.sex': ':maleSex'}},
+          {$gt: {'staff.age': ':maleAge'}}
+        ]
+      },
+      {
+        // Female and over the age of 23.
+        $and: [
+          {$eq: {'staff.sex': ':femaleSex'}},
+          {$gt: {'staff.age': ':femaleAge'}}
+        ]
+      }
     ]
-  },
-  {male: 'male', female: 'female'})
-  .select('staff.staffID', 'staff.firstName', 'staff.lastName', 'staff.sex', 'staff.age');
+  };
 
-console.log('Query:');
-console.log(query.toString(), '\n');
+  const params = {
+    maleSex:   'male',
+    maleAge:   25,
+    femaleSex: 'female',
+    femaleAge: 23
+  };
 
-query.execute()
-  .then(function(result)
-  {
-    console.log('Result:');
-    console.log(result);
-  })
-  .catch(function(err)
-  {
-    console.log(err);
-  })
-  .finally(function()
-  {
-    bikeShopDC.getQueryExecuter().getConnectionPool().end();
-  });
+  const query = retrieveStaffQuery(dataContext)
+    .where(cond, params)
+    .select()
+    .orderBy('staff.sex', 'staff.age');
+
+  console.log('Query:');
+  console.log(query.toString(), '\n');
+
+  return query
+    .execute();
+}
+
+function printResult(result) {
+  console.log('Result:');
+  console.log(util.inspect(result, {depth: null}));
+}
+
